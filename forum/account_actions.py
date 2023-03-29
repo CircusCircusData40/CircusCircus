360aa5186c88409ce3f503d63797406fcddc9963
Adam-Barto <adam.jbarto@gmail.com>
2023-03-29 13:00:41 -0400

    Account Actions Added

---
+++ forum/account_actions.py
@@ -0,0 +1,56 @@
+from flask_login import login_user, logout_user
+
+from forum.page_generator import *
+from forum.database_checks import *
+
+
+@app.route('/action_login', methods=['POST'])
+def action_login():
+    username = request.form['username']
+    password = request.form['password']
+    user = User.query.filter(User.username == username).first()
+    if user and user.check_password(password):
+        login_user(user)
+    else:
+        errors = []
+        errors.append("Username or password is incorrect!")
+        return render_template("login.html", errors=errors)
+    return redirect("/")
+
+
+@login_required
+@app.route('/action_logout')
+def action_logout():
+    # todo
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/action_createaccount', methods=['POST'])
+def action_createaccount():
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
+    errors = []
+    retry = False
+    if username_taken(username):
+        errors.append("Username is already taken!")
+        retry = True
+    if email_taken(email):
+        errors.append("An account already exists with this email!")
+        retry = True
+    if not valid_username(username):
+        errors.append("Username is not valid!")
+        retry = True
+    # if not valid_password(password):
+    # 	errors.append("Password is not valid!")
+    # 	retry = True
+    if retry:
+        return render_template("login.html", errors=errors)
+    user = User(email, username, password)
+    if user.username == "admin":
+        user.admin = True
+    db.session.add(user)
+    db.session.commit()
+    login_user(user)
+    return redirect("/")
--- forum/forum.py
+++ forum/forum.py
@@ -15,7 +15,6 @@ import datetime
 from flask_login.login_manager import LoginManager
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from forum.object_models import *
 from forum.page_generator import *
 from forum.database_checks import *
 # adding db url
@@ -123,55 +122,55 @@ def action_post():
 	return redirect("/viewpost?post=" + str(post.id))
 
 
-@app.route('/action_login', methods=['POST'])
-def action_login():
-	username = request.form['username']
-	password = request.form['password']
-	user = User.query.filter(User.username == username).first()
-	if user and user.check_password(password):
-		login_user(user)
-	else:
-		errors = []
-		errors.append("Username or password is incorrect!")
-		return render_template("login.html", errors=errors)
-	return redirect("/")
-
-
-@login_required
-@app.route('/action_logout')
-def action_logout():
-	#todo
-	logout_user()
-	return redirect("/")
-
-@app.route('/action_createaccount', methods=['POST'])
-def action_createaccount():
-	username = request.form['username']
-	password = request.form['password']
-	email = request.form['email']
-	errors = []
-	retry = False
-	if username_taken(username):
-		errors.append("Username is already taken!")
-		retry=True
-	if email_taken(email):
-		errors.append("An account already exists with this email!")
-		retry = True
-	if not valid_username(username):
-		errors.append("Username is not valid!")
-		retry = True
-	# if not valid_password(password):
-	# 	errors.append("Password is not valid!")
-	# 	retry = True
-	if retry:
-		return render_template("login.html", errors=errors)
-	user = User(email, username, password)
-	if user.username == "admin":
-		user.admin = True
-	db.session.add(user)
-	db.session.commit()
-	login_user(user)
-	return redirect("/")
+# @app.route('/action_login', methods=['POST'])
+# def action_login():
+# 	username = request.form['username']
+# 	password = request.form['password']
+# 	user = User.query.filter(User.username == username).first()
+# 	if user and user.check_password(password):
+# 		login_user(user)
+# 	else:
+# 		errors = []
+# 		errors.append("Username or password is incorrect!")
+# 		return render_template("login.html", errors=errors)
+# 	return redirect("/")
+#
+#
+# @login_required
+# @app.route('/action_logout')
+# def action_logout():
+# 	#todo
+# 	logout_user()
+# 	return redirect("/")
+#
+# @app.route('/action_createaccount', methods=['POST'])
+# def action_createaccount():
+# 	username = request.form['username']
+# 	password = request.form['password']
+# 	email = request.form['email']
+# 	errors = []
+# 	retry = False
+# 	if username_taken(username):
+# 		errors.append("Username is already taken!")
+# 		retry=True
+# 	if email_taken(email):
+# 		errors.append("An account already exists with this email!")
+# 		retry = True
+# 	if not valid_username(username):
+# 		errors.append("Username is not valid!")
+# 		retry = True
+# 	# if not valid_password(password):
+# 	# 	errors.append("Password is not valid!")
+# 	# 	retry = True
+# 	if retry:
+# 		return render_template("login.html", errors=errors)
+# 	user = User(email, username, password)
+# 	if user.username == "admin":
+# 		user.admin = True
+# 	db.session.add(user)
+# 	db.session.commit()
+# 	login_user(user)
+# 	return redirect("/")
 
 # def error(errormessage):
 # 	return "<b style=\"color: red;\">" + errormessage + "</b>"
