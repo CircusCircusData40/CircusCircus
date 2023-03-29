8f39eea59f7242e18eb3865010075e516c9918d3
Adam-Barto <adam.jbarto@gmail.com>
2023-03-29 13:08:23 -0400

    Account Actions Updated

--- forum/account_actions.py
+++ forum/account_actions.py
@@ -1,7 +1,9 @@
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, current_user
 
 from forum.page_generator import *
 from forum.database_checks import *
+
+current_user = LocalProxy(lambda: _get_user())
 
 
 @app.route('/action_login', methods=['POST'])
@@ -21,7 +23,7 @@ def action_login():
 @login_required
 @app.route('/action_logout')
 def action_logout():
-    # todo
+    # to do
     logout_user()
     return redirect("/")
 
@@ -54,3 +56,48 @@ def action_createaccount():
     db.session.commit()
     login_user(user)
     return redirect("/")
+
+
+@login_required
+@app.route('/action_post', methods=['POST'])
+def action_post():
+    subforum_id = int(request.args.get("sub"))
+    subforum = Subforum.query.filter(Subforum.id == subforum_id).first()
+    if not subforum:
+        return redirect(url_for("subforums"))
+
+    user = current_user
+    title = request.form['title']
+    content = request.form['content']
+    # check for valid posting
+    errors = []
+    retry = False
+    if not valid_title(title):
+        errors.append("Title must be between 4 and 140 characters long!")
+        retry = True
+    if not valid_content(content):
+        errors.append("Post must be between 10 and 5000 characters long!")
+        retry = True
+    if retry:
+        return render_template("createpost.html", subforum=subforum, errors=errors)
+    post = Post(title, content, datetime.datetime.now())
+    subforum.posts.append(post)
+    user.posts.append(post)
+    db.session.commit()
+    return redirect("/viewpost?post=" + str(post.id))
+
+
+@login_required
+@app.route('/action_comment', methods=['POST', 'GET'])
+def comment():
+    post_id = int(request.args.get("post"))
+    post = Post.query.filter(Post.id == post_id).first()
+    if not post:
+        return error("That post does not exist!")
+    content = request.form['content']
+    postdate = datetime.datetime.now()
+    comment = Comment(content, postdate)
+    current_user.comments.append(comment)
+    post.comments.append(comment)
+    db.session.commit()
+    return redirect("/viewpost?post=" + str(post_id))
--- forum/forum.py
+++ forum/forum.py
@@ -1,19 +1,19 @@
-from flask import *
-#from flask.ext.login import LoginManager, login_required, current_user, logout_user, login_user
-from flask_login import LoginManager, current_user, login_user, logout_user
-import datetime
-
-# Test 2
-
-from flask_login.utils import login_required
-from forum.app import app
-from flask_sqlalchemy import SQLAlchemy
-
-from flask_login import UserMixin
-import re
-import datetime
-from flask_login.login_manager import LoginManager
-from werkzeug.security import generate_password_hash, check_password_hash
+# from flask import *
+# from flask.ext.login import LoginManager, login_required, current_user, logout_user, login_user
+# from flask_login import LoginManager, current_user, login_user, logout_user
+# import datetime
+#
+# # Test 2
+#
+# from flask_login.utils import login_required
+# from forum.app import app
+# from flask_sqlalchemy import SQLAlchemy
+#
+# from flask_login import UserMixin
+# import re
+# import datetime
+# from flask_login.login_manager import LoginManager
+# from werkzeug.security import generate_password_hash, check_password_hash
 
 from forum.page_generator import *
 from forum.database_checks import *
@@ -78,49 +78,49 @@ datat31 = "good people"
 
 #ACTIONS
 
-@login_required
-@app.route('/action_comment', methods=['POST', 'GET'])
-def comment():
-	post_id = int(request.args.get("post"))
-	post = Post.query.filter(Post.id == post_id).first()
-	if not post:
-		return error("That post does not exist!")
-	content = request.form['content']
-	postdate = datetime.datetime.now()
-	comment = Comment(content, postdate)
-	current_user.comments.append(comment)
-	post.comments.append(comment)
-	db.session.commit()
-	return redirect("/viewpost?post=" + str(post_id))
+# @login_required
+# @app.route('/action_comment', methods=['POST', 'GET'])
+# def comment():
+# 	post_id = int(request.args.get("post"))
+# 	post = Post.query.filter(Post.id == post_id).first()
+# 	if not post:
+# 		return error("That post does not exist!")
+# 	content = request.form['content']
+# 	postdate = datetime.datetime.now()
+# 	comment = Comment(content, postdate)
+# 	current_user.comments.append(comment)
+# 	post.comments.append(comment)
+# 	db.session.commit()
+# 	return redirect("/viewpost?post=" + str(post_id))
 
-@login_required
-@app.route('/action_post', methods=['POST'])
-def action_post():
-	subforum_id = int(request.args.get("sub"))
-	subforum = Subforum.query.filter(Subforum.id == subforum_id).first()
-	if not subforum:
-		return redirect(url_for("subforums"))
-
-	user = current_user
-	title = request.form['title']
-	content = request.form['content']
-	#check for valid posting
-	errors = []
-	retry = False
-	if not valid_title(title):
-		errors.append("Title must be between 4 and 140 characters long!")
-		retry = True
-	if not valid_content(content):
-		errors.append("Post must be between 10 and 5000 characters long!")
-		retry = True
-	if retry:
-		return render_template("createpost.html",subforum=subforum,  errors=errors)
-	post = Post(title, content, datetime.datetime.now())
-	subforum.posts.append(post)
-	user.posts.append(post)
-	db.session.commit()
-	return redirect("/viewpost?post=" + str(post.id))
-
+# @login_required
+# @app.route('/action_post', methods=['POST'])
+# def action_post():
+# 	subforum_id = int(request.args.get("sub"))
+# 	subforum = Subforum.query.filter(Subforum.id == subforum_id).first()
+# 	if not subforum:
+# 		return redirect(url_for("subforums"))
+#
+# 	user = current_user
+# 	title = request.form['title']
+# 	content = request.form['content']
+# 	#check for valid posting
+# 	errors = []
+# 	retry = False
+# 	if not valid_title(title):
+# 		errors.append("Title must be between 4 and 140 characters long!")
+# 		retry = True
+# 	if not valid_content(content):
+# 		errors.append("Post must be between 10 and 5000 characters long!")
+# 		retry = True
+# 	if retry:
+# 		return render_template("createpost.html",subforum=subforum,  errors=errors)
+# 	post = Post(title, content, datetime.datetime.now())
+# 	subforum.posts.append(post)
+# 	user.posts.append(post)
+# 	db.session.commit()
+# 	return redirect("/viewpost?post=" + str(post.id))
+#
 
 # @app.route('/action_login', methods=['POST'])
 # def action_login():
@@ -139,7 +139,7 @@ def action_post():
 # @login_required
 # @app.route('/action_logout')
 # def action_logout():
-# 	#todo
+# 	#to do
 # 	logout_user()
 # 	return redirect("/")
 #
