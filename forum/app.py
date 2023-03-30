77dafbeb77fabe6739c164d50f4ec2105f7350ed
joe-trung <kieutrungtran@gmail.com>
2023-03-29 22:34:59 -0400

    Done Bootstrap

--- forum/app.py
+++ forum/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 app.config.update(
     TESTING=True,
     SECRET_KEY=b'kristofer',
-	SITE_NAME = "COOL DATA 40",
+	SITE_NAME = "ZIPCODE FORUM",
 	SITE_DESCRIPTION = "THE VOICE OF COOL DATA 40 COHORT",
-	SQLALCHEMY_DATABASE_URI='sqlite:////tmp/database.db'
+	SQLALCHEMY_DATABASE_URI='sqlite:////tmp/database.db',
+	SQLALCHEMY_ECHO=True
 )
