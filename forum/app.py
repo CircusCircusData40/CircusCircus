d5f08da6cd08c4407c87b55e2ab27a8d669e8b9b
joe-trung <kieutrungtran@gmail.com>
2023-03-30 07:44:03 -0400

    working on usersettings

--- forum/app.py
+++ forum/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 app.config.update(
@@ -21,6 +21,14 @@ def instructor():
     return render_template("instructor.html")
 
 
-@app.route('/usersettings')
+@app.route('/usersettings', methods=["POST", "GET"])
 def usersettings():
-    return render_template("usersettings.html")
+    # if request.method == "POST":
+    #     name = request.form[name]
+    #     email = request.form[email]
+    #     gender = request.form[gender]
+    #     age = request.form[age]
+    #     comments = request.form[comments]
+    #
+    # else:
+        return render_template("usersettings.html")
