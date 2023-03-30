af22792e423cb5347c98cceb76327e42e7114773
joe-trung <kieutrungtran@gmail.com>
2023-03-29 23:31:50 -0400

    try to get team and instructor

--- forum/app.py
+++ forum/app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.update(
     TESTING=True,
     SECRET_KEY=b'kristofer',
-	SITE_NAME = "ZIPCODE FORUM",
-	SITE_DESCRIPTION = "THE VOICE OF COOL DATA 40 COHORT",
-	SQLALCHEMY_DATABASE_URI='sqlite:////tmp/database.db',
-	SQLALCHEMY_ECHO=True
+    SITE_NAME="ZIPCODE FORUM",
+    SITE_DESCRIPTION="THE VOICE OF COOL DATA 40 COHORT",
+    SQLALCHEMY_DATABASE_URI='sqlite:////tmp/database.db',
+    SQLALCHEMY_ECHO=True
 )
+
+
+@app.route('/team')
+def team():
+    return render_template("team.html")
+
+
+@app.route('/instructor')
+def instructor():
+    return render_template("instructor.html")
